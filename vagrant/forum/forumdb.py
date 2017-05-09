426fa8a8e71c5b8f220a39ae49b97ee599c1b737
Cheryl Spanos <chspanos@comcast.net>
2017-05-09 16:02:18 -0700

    init: Initial commit to setup fullstack VM repo.

---
+++ vagrant/forum/forumdb.py
@@ -0,0 +1,24 @@
+# "Database code" for the DB Forum.
+
+import psycopg2
+import bleach
+
+# POSTS = [("This is the first post.", datetime.datetime.now())]
+
+def get_posts():
+  """Return all posts from the 'database', most recent first."""
+  db = psycopg2.connect("dbname=forum")
+  c = db.cursor()
+  c.execute("select content, time from posts order by time desc;")
+  posts = c.fetchall()
+  db.close()
+  return posts
+
+def add_post(content):
+  """Add a post to the 'database' with the current timestamp."""
+  db = psycopg2.connect("dbname=forum")
+  c = db.cursor()
+  clean_content = bleach.clean(content)
+  c.execute("insert into posts (content) values (%s)", (clean_content,))
+  db.commit()
+  db.close()
